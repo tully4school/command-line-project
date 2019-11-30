d6d3c81078d7c5695a3b61230ce693ebc1ff2828
Joseph_Tully <tully4school@gmail.com>
2019-11-30 12:19:22 -0500

    completed. you can search by any letter in any name

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from peewee imoport *
+from peewee import Model, CharField, DateField, PostgresqlDatabase
 from datetime import date
 import sys
 
-db = PostgresqlDatabase('people', user='postgres',
+db = PostgresqlDatabase('blackbook', user='postgres',
                         password='', host='localhost', port=5432)
 
 
@@ -18,6 +18,7 @@ class Person(BaseModel):
 
 
 db.connect()
+db.create_tables([Person])
 
 res = input("would you like to create a new contact? (y / n)")
 if res == 'y':
@@ -26,17 +27,18 @@ if res == 'y':
     new_month = int(input("what month were they born? "))
     new_day = int(input("what day were they born? "))
     new_phone = int(input("what is their phone number? "))
-    new_contact = Person(name=new_name, birthday=date(new_year, new_month, new_day) phone=new_phone)
+    new_contact = Person(name=new_name, birthday=date(
+        new_year, new_month, new_day), phone=new_phone)
     new_contact.save()  # new person is stored in the db
-    else:
-        pass
+    print(
+        f"\n\n\nYOUR NEW CONTACT HAS BEEN SAVED!\n{new_contact.name}\n{new_contact.birthday}\n{new_contact.phone}\n\n\n")
+else:
+    pass
 
-res = input("would you like to search by name? (y / n)")
-if res == y:
-    search = input("search by name: ")
-    output = Person.select().where(Person.name == search).get()
-    print(output.name)
-    print(output.phone)
-    print(output.birthday)
-    else:
-        pass
+res = input(f"would you like to search by name? (y / n) ")
+if res == 'y':
+    search = input(f"\n\nsearch by name: ")
+    output = Person.select().where(Person.name.contains(search)).get()
+    print(f"\n{output.name}\n{output.phone}\n{output.birthday}\n")
+else:
+    print(f"\n\nThank you for using your Little Black Book \n \n")
