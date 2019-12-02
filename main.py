4c47556cd5334119a58c10cfa00249382a192c71
Joseph_Tully <tully4school@gmail.com>
2019-12-02 17:34:14 -0500

    added a view all contacts portion

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Person(BaseModel):
 db.connect()
 db.create_tables([Person])
 
-res = input("would you like to create a new contact? (y / n) ")
+res = input(f"\n\nwould you like to create a new contact? (y / n) ")
 if res == 'y':
     new_name = input("add a name: ")
     new_year = int(input("what year were they born? "))
@@ -34,11 +34,16 @@ if res == 'y':
         f"\n\n\nYOUR NEW CONTACT HAS BEEN SAVED!\n{new_contact.name}\n{new_contact.birthday}\n{new_contact.phone}\n\n\n")
 else:
     pass
-
-res = input(f"would you like to search by name? (y / n) ")
+res = input(f"\n\nwould you like to search by name? (y / n) ")
 if res == 'y':
     search = input(f"\n\nsearch by name: ")
     output = Person.select().where(Person.name.contains(search)).get()
     print(f"\n{output.name}\n{output.phone}\n{output.birthday}\n")
+else:
+    pass
+res = input(f"\n\nWould you like to view all of your contacts? (y / n) ")
+if res == 'y':
+    for person in Person.select():
+        print(f"{person.name}\n{person.phone}\n{person.birthday}\n\n\n")
 else:
     print(f"\n\nThank you for using your Little Black Book \n \n")
